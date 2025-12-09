734eaa845f9a2a935394ab2fe81dc0debb297675
Jhonny <carvalhoborgeshenrique@gmail.com>
2025-12-09 11:23:02 -0300

    Distributividade prop I

--- Analise/1_TheoremProvingInLean4/props_and_proves.lean
+++ Analise/1_TheoremProvingInLean4/props_and_proves.lean
@@ -3,12 +3,11 @@ variable (p q r : Prop)
 -- commutativity of ∧ and ∨
 example : p ∧ q ↔ q ∧ p :=
   Iff.intro
-    (
+    ( -- p ∧ q → q ∧ p
       fun h : p ∧ q =>
       show q ∧ p from And.intro (And.right h) ( And.left h )
     )
-
-    (
+    ( -- q ∧ p → p ∧ q
       fun h : q ∧ p =>
       show p ∧ q from And.intro (And.right h) (And.left h)
     )
@@ -91,23 +90,57 @@ example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) :=
     )
   )
 
-
-
-
-
 -- distributivity
-example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := sorry
+example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) :=
+  Iff.intro
+  (
+    fun h : p ∧ (q ∨ r) =>
+    have hp : p := h.left
+    have hqr : q ∨ r := h.right
+    Or.elim hqr
+    (
+      fun hq : q =>
+        show (p ∧ q) ∨ (p ∧ r) from Or.intro_left (p ∧ r) (And.intro hp hq)
+    )
+    (
+      fun hr : r =>
+        show (p ∧ q) ∨ (p ∧ r) from Or.intro_right (p ∧ q) (And.intro hp hr)
+    )
+  )
+  (
+    fun h : (p ∧ q) ∨ (p ∧ r) =>
+    Or.elim h
+    (
+      fun hpq : p ∧ q =>
+      show p ∧ (q ∨ r) from And.intro hpq.left (Or.intro_left r (hpq.right))
+    )
+    (
+      fun hpr : p ∧ r =>
+      show p ∧ (q ∨ r) from And.intro hpr.left (Or.intro_right q (hpr.right))
+    )
+  )
+
 example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
 
 -- other properties
 example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
+
 example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
+
 example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
+
 example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
+
 example : ¬(p ∧ ¬p) := sorry
+
 example : p ∧ ¬q → ¬(p → q) := sorry
+
 example : ¬p → (p → q) := sorry
+
 example : (¬p ∨ q) → (p → q) := sorry
+
 example : p ∨ False ↔ p := sorry
+
 example : p ∧ False ↔ False := sorry
+
 example : (p → q) → (¬q → ¬p) := sorry
